a7780cdf45f5794bb56e0dd7466ed03f12caf25d
eimwe <prokopenkoserge@inbox.ru>
2024-12-08 01:22:18 +0500

    Displayed db entries in clients tab

--- admin_workstation/MainForm.Designer.cs
+++ admin_workstation/MainForm.Designer.cs
@@ -30,13 +30,13 @@
         {
             tabControlTables = new TabControl();
             tabPageClients = new TabPage();
-            btnDeleteClient = new Button();
-            btnUpdateClient = new Button();
+            dataGridViewClients = new DataGridView();
             btnAddClient = new Button();
-            dataGridView1 = new DataGridView();
+            btnUpdateClient = new Button();
+            btnDeleteClient = new Button();
             tabControlTables.SuspendLayout();
             tabPageClients.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewClients).BeginInit();
             SuspendLayout();
             // 
             // tabControlTables
@@ -48,46 +48,36 @@
             tabControlTables.Multiline = true;
             tabControlTables.Name = "tabControlTables";
             tabControlTables.SelectedIndex = 0;
-            tabControlTables.Size = new Size(776, 426);
+            tabControlTables.Size = new Size(776, 408);
             tabControlTables.TabIndex = 0;
             // 
             // tabPageClients
             // 
-            tabPageClients.Controls.Add(dataGridView1);
+            tabPageClients.Controls.Add(dataGridViewClients);
             tabPageClients.Controls.Add(btnAddClient);
             tabPageClients.Controls.Add(btnUpdateClient);
             tabPageClients.Controls.Add(btnDeleteClient);
             tabPageClients.Location = new Point(4, 30);
             tabPageClients.Name = "tabPageClients";
             tabPageClients.Padding = new Padding(3);
-            tabPageClients.Size = new Size(768, 392);
+            tabPageClients.Size = new Size(768, 374);
             tabPageClients.TabIndex = 0;
             tabPageClients.Text = "Clients";
             tabPageClients.UseVisualStyleBackColor = true;
             // 
-            // btnDeleteClient
+            // dataGridViewClients
             // 
-            btnDeleteClient.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnDeleteClient.AutoSize = true;
-            btnDeleteClient.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            btnDeleteClient.Location = new Point(678, 6);
-            btnDeleteClient.Name = "btnDeleteClient";
-            btnDeleteClient.Size = new Size(84, 25);
-            btnDeleteClient.TabIndex = 0;
-            btnDeleteClient.Text = "Delete Client";
-            btnDeleteClient.UseVisualStyleBackColor = true;
-            // 
-            // btnUpdateClient
-            // 
-            btnUpdateClient.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnUpdateClient.AutoSize = true;
-            btnUpdateClient.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            btnUpdateClient.Location = new Point(583, 6);
-            btnUpdateClient.Name = "btnUpdateClient";
-            btnUpdateClient.Size = new Size(89, 25);
-            btnUpdateClient.TabIndex = 0;
-            btnUpdateClient.Text = "Update Client";
-            btnUpdateClient.UseVisualStyleBackColor = true;
+            dataGridViewClients.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridViewClients.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewClients.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewClients.Location = new Point(6, 37);
+            dataGridViewClients.MultiSelect = false;
+            dataGridViewClients.Name = "dataGridViewClients";
+            dataGridViewClients.RowHeadersVisible = false;
+            dataGridViewClients.RowTemplate.Height = 25;
+            dataGridViewClients.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewClients.Size = new Size(756, 331);
+            dataGridViewClients.TabIndex = 1;
             // 
             // btnAddClient
             // 
@@ -101,32 +91,42 @@
             btnAddClient.Text = "Add Client";
             btnAddClient.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // btnUpdateClient
             // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(6, 37);
-            dataGridView1.MultiSelect = false;
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersVisible = false;
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView1.Size = new Size(756, 349);
-            dataGridView1.TabIndex = 1;
+            btnUpdateClient.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnUpdateClient.AutoSize = true;
+            btnUpdateClient.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnUpdateClient.Location = new Point(583, 6);
+            btnUpdateClient.Name = "btnUpdateClient";
+            btnUpdateClient.Size = new Size(89, 25);
+            btnUpdateClient.TabIndex = 0;
+            btnUpdateClient.Text = "Update Client";
+            btnUpdateClient.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteClient
+            // 
+            btnDeleteClient.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnDeleteClient.AutoSize = true;
+            btnDeleteClient.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDeleteClient.Location = new Point(678, 6);
+            btnDeleteClient.Name = "btnDeleteClient";
+            btnDeleteClient.Size = new Size(84, 25);
+            btnDeleteClient.TabIndex = 0;
+            btnDeleteClient.Text = "Delete Client";
+            btnDeleteClient.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 432);
             Controls.Add(tabControlTables);
             Name = "MainForm";
             Text = "Language Center Admin Workstation";
             tabControlTables.ResumeLayout(false);
             tabPageClients.ResumeLayout(false);
             tabPageClients.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewClients).EndInit();
             ResumeLayout(false);
         }
 
@@ -134,7 +134,7 @@
 
         private TabControl tabControlTables;
         private TabPage tabPageClients;
-        private DataGridView dataGridView1;
+        private DataGridView dataGridViewClients;
         private Button btnAddClient;
         private Button btnUpdateClient;
         private Button btnDeleteClient;
--- admin_workstation/MainForm.cs
+++ admin_workstation/MainForm.cs
@@ -1,3 +1,6 @@
+using admin_workstation.Repositories;
+using System.Data;
+
 namespace admin_workstation
 {
     public partial class MainForm : Form
@@ -5,6 +8,36 @@ namespace admin_workstation
         public MainForm()
         {
             InitializeComponent();
+            ReadClients();
+        }
+
+        private void ReadClients()
+        {
+            DataTable dataTable = new DataTable();
+
+            dataTable.Columns.Add("ID");
+            dataTable.Columns.Add("Name");
+            dataTable.Columns.Add("Date of Birth");
+            dataTable.Columns.Add("Phone");
+
+            var repo = new ClientRepository();
+            var clients = repo.GetClients();
+
+            Console.WriteLine(clients.Count);
+
+            foreach (var client in clients)
+            {
+                var row = dataTable.NewRow();
+
+                row["ID"] = client.id;
+                row["Name"] = client.firstName + " " + client.lastName;
+                row["Date of Birth"] = client.birthDate;
+                row["Phone"] = client.phone;
+
+                dataTable.Rows.Add(row);
+            }
+
+            this.dataGridViewClients.DataSource = dataTable;
         }
     }
 }
