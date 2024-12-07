7db0f7fed1b0eab3596f4f9ecd5c43edb39296c4
eimwe <prokopenkoserge@inbox.ru>
2024-12-07 19:11:59 +0500

    Created 'Client' class

---
+++ admin_workstation/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Models
+{
+    public class Client
+    {
+        public int id;
+        public string firstName = "";
+        public string lastName = "";
+        public string birthDate = "";
+        public string phone = "";
+    }
+}
