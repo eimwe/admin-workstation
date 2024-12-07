b0937f5b469024421fedf90c55c892612ee5656c
eimwe <prokopenkoserge@inbox.ru>
2024-12-07 23:36:53 +0500

    Added tab component

--- admin_workstation/MainForm.Designer.cs
+++ admin_workstation/MainForm.Designer.cs
@@ -28,18 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tabControlTables = new TabControl();
+            tabPageClients = new TabPage();
+            tabControlTables.SuspendLayout();
             SuspendLayout();
+            // 
+            // tabControlTables
+            // 
+            tabControlTables.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tabControlTables.Controls.Add(tabPageClients);
+            tabControlTables.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            tabControlTables.Location = new Point(12, 12);
+            tabControlTables.Multiline = true;
+            tabControlTables.Name = "tabControlTables";
+            tabControlTables.SelectedIndex = 0;
+            tabControlTables.Size = new Size(776, 426);
+            tabControlTables.TabIndex = 0;
+            // 
+            // tabPageClients
+            // 
+            tabPageClients.Location = new Point(4, 30);
+            tabPageClients.Name = "tabPageClients";
+            tabPageClients.Padding = new Padding(3);
+            tabPageClients.Size = new Size(768, 392);
+            tabPageClients.TabIndex = 0;
+            tabPageClients.Text = "Clients";
+            tabPageClients.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(tabControlTables);
             Name = "MainForm";
             Text = "Language Center Admin Workstation";
+            tabControlTables.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControlTables;
+        private TabPage tabPageClients;
     }
 }
