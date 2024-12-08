d726ea4d68ec1cdf2e73981e647aaf73d590099c
eimwe <prokopenkoserge@inbox.ru>
2024-12-08 11:26:06 +0500

    Created add/edit client modal template

---
+++ admin_workstation/CreateUpdateForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace admin_workstation
+{
+    partial class CreateUpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelTitle = new Label();
+            SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            labelTitle.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelTitle.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelTitle.Location = new Point(12, 9);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(776, 24);
+            labelTitle.TabIndex = 0;
+            labelTitle.Text = "Create Client";
+            labelTitle.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // CreateUpdateForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(labelTitle);
+            Name = "CreateUpdateForm";
+            Text = "Manage Client";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label labelTitle;
+    }
+}
