97d795390840025347d3fc93e2bb65d0e71cebd2
eimwe <prokopenkoserge@inbox.ru>
2024-12-08 13:37:03 +0500

    Added method to update client info

--- admin_workstation/CreateUpdateForm.cs
+++ admin_workstation/CreateUpdateForm.cs
@@ -20,17 +20,40 @@ namespace admin_workstation
             this.DialogResult = DialogResult.Cancel;
         }
 
+        private int clientId = 0;
+
+        public void UpdateClient(Client client)
+        {
+            this.Text = "Update Client";
+            this.labelTitle.Text = "Update Client";
+
+            this.labelClientId.Text = "" + client.id;
+            this.textBoxClientFirstName.Text = client.firstName;
+            this.textBoxClientLastName.Text = client.lastName;
+            this.textBoxClientBirthDate.Text = client.birthDate;
+            this.textBoxClientPhone.Text = client.phone;
+
+            this.clientId = client.id;
+        }
+
         private void btnSave_Click(object sender, EventArgs e)
         {
             Client client = new Client();
-            client.id = 0;
+            client.id = this.clientId;
             client.firstName = this.textBoxClientFirstName.Text;
             client.lastName = this.textBoxClientLastName.Text;
             client.birthDate = this.textBoxClientBirthDate.Text;
             client.phone = this.textBoxClientPhone.Text;
 
             var repo = new ClientRepository();
-            repo.CreateClient(client);
+
+            if (client.id == 0)
+            {
+                repo.CreateClient(client);
+            } else
+            {
+                repo.UpdateClient(client);
+            }
 
             this.DialogResult = DialogResult.OK;
         }
