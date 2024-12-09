a131173cd65417447d6bc0e9e893980dd92adad2
eimwe <prokopenkoserge@inbox.ru>
2024-12-09 15:44:29 +0500

    Added Course model

---
+++ admin_workstation/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Models
+{
+    public class Course
+    {
+        public int id;
+        public string title = "";
+        public string about = "";
+        public int duration;
+        public int totalprice;
+        public int monthprice;
+    }
+}
