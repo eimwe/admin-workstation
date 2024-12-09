30cc4a5b37095021514a93c35d4a21be92ebc6f2
eimwe <prokopenkoserge@inbox.ru>
2024-12-09 15:27:56 +0500

    Added Payment model

---
+++ admin_workstation/Models/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Models
+{
+    public class Payment
+    {
+        public int id;
+        public int clientId;
+        public int courseId;
+        public decimal amount = 0;
+        public DateTime paymentDate = DateTime.Now;
+    }
+}
