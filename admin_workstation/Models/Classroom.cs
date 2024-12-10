8d6678cb998015bc5c8a580ac57abc04f89fa95a
eimwe <prokopenkoserge@inbox.ru>
2024-12-11 00:53:10 +0500

    Added Classroom class

---
+++ admin_workstation/Models/Classroom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Models
+{
+    public class Classroom
+    {
+        public int id;
+        public string room = "";
+    }
+}
