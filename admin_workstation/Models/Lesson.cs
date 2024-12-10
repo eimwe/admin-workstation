ca58dbcabe3bd6124bd489132ef8c36201a7bfb6
eimwe <prokopenkoserge@inbox.ru>
2024-12-11 00:48:32 +0500

    Added Lesson class

---
+++ admin_workstation/Models/Lesson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Models
+{
+    public class Lesson
+    {
+        public int id;
+        public int clientId;
+        public string clientName = "";
+        public int courseId;
+        public string courseTitle = "";
+        public int teacherId;
+        public string teacherName = "";
+        public int classroomId;
+        public string classroomNumber = "";
+        public DateTime lessonDate = DateTime.Now;
+    }
+}
