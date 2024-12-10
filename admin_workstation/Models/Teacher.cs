35e9ac1f7b3f431c1381f3d0200176e240866315
eimwe <prokopenkoserge@inbox.ru>
2024-12-11 00:51:42 +0500

    Added Teacher class

---
+++ admin_workstation/Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Models
+{
+    public class Teacher
+    {
+        public int id;
+        public string firstName = "";
+        public string lastName = "";
+        public string userName = "";
+        public string passKey = "";
+        public string birthDate = "";
+        public string phone = "";
+    }
+}
