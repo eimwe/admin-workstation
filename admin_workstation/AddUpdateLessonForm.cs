525e4cd591e272447f86dc61965c33d20636e69c
eimwe <prokopenkoserge@inbox.ru>
2024-12-11 09:15:05 +0500

    Added lesson form combobox loader

--- admin_workstation/AddUpdateLessonForm.cs
+++ admin_workstation/AddUpdateLessonForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using admin_workstation.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,35 @@ namespace admin_workstation
         public AddUpdateLessonForm()
         {
             InitializeComponent();
+            LoadComboBoxData();
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void LoadComboBoxData()
+        {
+            var clientRepo = new ClientRepository();
+            var clients = clientRepo.GetClients();
+            this.comboBoxLessonClients.DataSource = clients.Select(c => new { Value = c.id, Display = c.firstName + " " + c.lastName }).ToList();
+            this.comboBoxLessonClients.DisplayMember = "Display";
+            this.comboBoxLessonClients.ValueMember = "Value";
+
+            var courseRepo = new CourseRepository();
+            var courses = courseRepo.GetCourses();
+            this.comboBoxLessonCourses.DataSource = courses.Select(c => new { Value = c.id, Display = c.title }).ToList();
+            this.comboBoxLessonCourses.DisplayMember = "Display";
+            this.comboBoxLessonCourses.ValueMember = "Value";
+
+            var teacherRepo = new TeacherRepository();
+            var teachers = teacherRepo.GetTeachers();
+            this.comboBoxLessonTeachers.DataSource = teachers.Select(c => new { Value = c.id, Display = c.firstName + " " + c.lastName }).ToList();
+            this.comboBoxLessonTeachers.DisplayMember = "Display";
+            this.comboBoxLessonTeachers.ValueMember = "Value";
+
+            var classroomRepo = new ClassroomRepository();
+            var classrooms = classroomRepo.GetClassrooms();
+            this.comboBoxLessonClassroom.DataSource = classrooms.Select(c => new { Value = c.id, Display = c.room }).ToList();
+            this.comboBoxLessonClassroom.DisplayMember = "Display";
+            this.comboBoxLessonClassroom.ValueMember = "Value";
         }
     }
 }
