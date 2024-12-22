d1f33e04ef67b55d28fa043bed05d88ec4c3a19b
eimwe <prokopenkoserge@inbox.ru>
2024-12-22 19:40:37 +0500

    Created database config To get connection string for both development and production environments

---
+++ admin_workstation/Configs/DatabaseConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Configs
+{
+    public static class DatabaseConfig
+    {
+        public static string GetConnectionString()
+        {
+            string dbPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "language-center.db"
+            );
+
+            return $"Data Source={dbPath};Version=3;";
+        }
+    }
+}
