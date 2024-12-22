c10fc38151dec4ed7107f6e7b72ef0f5e490a52f
eimwe <prokopenkoserge@inbox.ru>
2024-12-22 22:23:21 +0500

    Added database checker

---
+++ admin_workstation/Services/DatabaseCheck.cs
@@ -0,0 +1,35 @@
+﻿using admin_workstation.Configs;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admin_workstation.Services
+{
+    public class DatabaseCheck
+    {
+        public static bool VerifyDatabase()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(DatabaseConfig.GetConnectionString()))
+                {
+                    connection.Open();
+                    using (var cmd = new SQLiteCommand("SELECT COUNT(*) FROM clients", connection))
+                    {
+                        cmd.ExecuteScalar();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Database verification failed: {ex.Message}", "Database Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+    }
+}
